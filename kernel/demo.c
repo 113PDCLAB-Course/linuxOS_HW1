d251f0c9137797fa9f3a80a1796cdc61b8cb58b0
TheRiseOfDavid <david53133@gmail.com>
2024-10-31 02:20:27 +0800

    stage3

--- kernel/demo.c
+++ kernel/demo.c
@@ -25,9 +25,20 @@ SYSCALL_DEFINE1(demo, void *, ptr_addr)
     pgd = pgd_offset(current->mm, v_addr);
 
 #ifdef DEBUG
-    printk("pgd_val = 0x%lu\n", pgd->pgd);
+    printk("pgd_val = 0x%lx\n", pgd->pgd); // pgd_offset 的內容
+    printk("pgd_val = 0x%lx\n", *pgd);     // pgd_offset 的內容
+
+    printk("pgd_val = 0x%lx\n", pgd);              // pgd_offset 的位置。
+    printk("pgd_val = 0x%lx\n", current->mm->pgd); // pgd_offset 的位置。
+
     // pgd_index，找出當前 page gloab table index entry. https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/pgtable.h#L90
-    printk("pgd_index = %lu\n", pgd_index(v_addr));
+    // 關於 pgd_index 的解釋 https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/pgtable.h#L56
+    // #define pgd_index(a)  (((a) >> PGDIR_SHIFT) & (PTRS_PER_PGD - 1))
+    // pgd_index 的原理是將右邊的 39bit 右移掉，在與 255 做 &，保證數字必定在 255 以內符合 page table 2^9 架構
+    printk("pgd_index = %lx\n", pgd_index(v_addr));
+
+    printk("PGDIR_SHIFT = %lx\n", PGDIR_SHIFT);
+    printk("PTRS_PER_PGD = %lx\n", PTRS_PER_PGD - 1);
 #endif
 
     /*  在 x86-64 processors 使用 long mode 模式，採用 PAE 模式。
@@ -35,8 +46,34 @@ SYSCALL_DEFINE1(demo, void *, ptr_addr)
         共用到 48bits，高位元則等待被設計、擴展。
         9bit 的原因是因為，一個 page 4kb 共 2^12 byte.
         在 32bits processors 情況下，一個 word 是 4bytes 共用，能夠存 2^10 個
-        在 32bits processors 情況下，一個 word 是 8bytes 共用，能夠存 2^9 個
-        因此要觀察 pointer 的 48bits~39bits 是否與 index 一樣 。
+        在 64bits processors 情況下，一個 word 是 8bytes 共用，能夠存 2^9 個
     */
+
+    //     pud_t *pud;
+    //     pud = pud_offset(pgd, v_addr);
+    // #ifdef DEBUG
+    // 	printk("pud_val = 0x%lx\n", pud->pud); // pgd_offset 的內容
+    //     printk("pud_val = 0x%lx\n", pud); // pgd_offset 的位置
+
+    //     // pgd_index，找出當前 page gloab table index entry. https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/pgtable.h#L90
+    //     // 關於 pgd_index 的解釋 https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/pgtable.h#L56
+    // 	// printk("pgd_index = %lx\n", pgd_index(v_addr));
+    //     printk("pud_index = %lx\n", pud_index(v_addr));
+    // #endif
+
+    //     pmd_t *pmd;
+    //     pmd = pmd_offset(pud, v_addr);
+    // #ifdef DEBUG
+    // 	printk("pmd_val = 0x%lx\n", pmd->pmd); // pgd_offset 的內容
+    //     printk("pmd_val = 0x%lx\n", pmd); // pgd_offset 的位置
+
+    //     // pgd_index，找出當前 page gloab table index entry. https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/pgtable.h#L90
+    //     // 關於 pgd_index 的解釋 https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/pgtable.h#L56
+    // 	// printk("pgd_index = %lx\n", pgd_index(v_addr));
+    //     printk("pmd_index = %lx\n", pmd_index(v_addr));
+    // #endif
+
+    //     pte_t *pte;
+
     return pgd->pgd;
 }
