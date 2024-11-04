412855ab63dfe95970e0c6045e2f51bcab226002
TheRiseOfDavid <david53133@gmail.com>
2024-11-04 21:03:55 +0800

    dirty kernel

--- kernel/demo.c
+++ kernel/demo.c
@@ -1,6 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #define DEBUG
+#define QUICK_SEARCH
 
 // 使用 marco 的方式並透過 (<type>, <name>) 進行 map 來完成多個參數應用。 https://elixir.bootlin.com/linux/v6.1.51/source/include/linux/syscalls.h#L117
 // backtrace:
@@ -21,12 +22,14 @@ SYSCALL_DEFINE1(demo, void *, ptr_addr)
     // pgd_t 的結構 https://elixir.bootlin.com/linux/v6.11.5/source/arch/x86/include/asm/pgtable_types.h#L295
     // pgd_t->pgdval_t 的型態 https://elixir.bootlin.com/linux/v6.11.5/source/arch/x86/include/asm/pgtable_64_types.h#L18
 
+#ifndef QUICK_SEARCH
     // pgd_offset，可直接從 mm 去 get pgd_t https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/pgtable.h#L147.
     pgd = pgd_offset(current->mm, v_addr);
 
 #ifdef DEBUG
     printk("pgd_val = 0x%lx\n", pgd->pgd); // pgd_offset 的內容
     printk("pgd_val = 0x%lx\n", *pgd);     // pgd_offset 的內容
+    printk("pgd_val = 0x%lx\n", pgd_val(*pgd));
 
     printk("pgd_val = 0x%lx\n", pgd);              // pgd_offset 的位置。
     printk("pgd_val = 0x%lx\n", current->mm->pgd); // pgd_offset 的位置。
@@ -48,7 +51,6 @@ SYSCALL_DEFINE1(demo, void *, ptr_addr)
         在 32bits processors 情況下，一個 word 是 4bytes 共用，能夠存 2^10 個
         在 64bits processors 情況下，一個 word 是 8bytes 共用，能夠存 2^9 個
     */
-
     // x86, asm 的特殊架構，用意是 to find an entry in a page-table-directory.
     p4d_t *p4d;
     p4d = p4d_offset(pgd, v_addr);
@@ -58,28 +60,62 @@ SYSCALL_DEFINE1(demo, void *, ptr_addr)
 #ifdef DEBUG
     printk("pud_val = 0x%lx\n", pud->pud);
     printk("pud_val = 0x%lx\n", *pud);
+    printk("pud_val = 0x%lx\n", pud_val(*pud));
     printk("pud_index = %lx\n", pud_index(v_addr));
 #endif
-
     pmd_t *pmd;
     pmd = pmd_offset(pud, v_addr);
+#endif
+
+#ifdef QUICK_SEARCH
+    pmd_t *pmd;
+    pmd = pmd_off(current->mm, v_addr);
+#endif
+    if (pmd_none(pmd))
+    {
+        printk("doesn't have memory space for this virtual address\n");
+        return 0;
+    }
+
 #ifdef DEBUG
     printk("pmd_val = 0x%lx\n", pmd->pmd);
     printk("pmd_val = 0x%lx\n", *pmd);
+    printk("pmd_val = 0x%lx\n", pmd_val(*pmd));
+    printk("pmd_val with pmd_off_k method = 0x%lx\n", *pmd_off(current->mm, v_addr)); // pmd 內容
+
     // 在 kernel v6.11.5 中有提供 pmd_off， 可直接用 current->mm call 到 pmd_off.
-    printk("pmd_val with pmd_off_k method = 0x%lx\n", pmd_off(current->mm, v_addr));
+    printk("pmd_address with pmd_off_k method = 0x%lx\n", pmd_off(current->mm, v_addr)); // pmd 位置
     printk("pmd_index = %lx\n", pmd_index(v_addr));
+
+    printk("pmd_base 0x%lx\n", pud_pgtable(*pud));
 #endif
 
     pte_t *pte;
     pte = pte_offset_kernel(pmd, v_addr);
+
 #ifdef DEBUG
     printk("pte_val = 0x%lx\n", pte->pte);
     printk("pte_val = 0x%lx\n", *pte);
+    printk("pte_val = 0x%lx\n", pte_val(*pte));
     printk("pte_index = %lx\n", pte_index(v_addr));
+    printk("pte_base = 0x%lx\n", pmd_page_vaddr(*pmd));
 #endif
 
-    unsigned long offset = v_addr & (4096 - 1);
-    unsigned long p_addr = pte->pte + (offset << 3);
+    unsigned long offset = v_addr & ((unsigned long)4096 - (unsigned long)1); // 用 2^12-1 來保留最後的 12bit
+    /*
+    page table 是使用 0~64bit 紀錄資料，12~64 bit 資料是 physical page address.
+    0~11 bit 則是用於記這個 page table 的其他供用，例如 read/write, dirty, cache disable 之類的
+    page table 圖表網址連結：https://stackoverflow.com/questions/68025493/the-size-of-a-page-table-entry
+    */
+
+    // PAGE_MASK 用於 ignore page table 0~11bit 的 page 資訊。
+    // 將 offset 與 memory physical address 合併就是完整的 physical address
+    unsigned long p_addr = (pte->pte & PAGE_MASK) | offset;
+
+#ifdef DEBUG
+    printk("(pte->pte & PAGE_MASK) = 0x%lx\n", (pte->pte & PAGE_MASK));
+    printk("(pte->pte & PAGE_MASK) = %p\n", (pte->pte & PAGE_MASK));
+    printk("p_addr = 0x%lx\n", p_addr);
+#endif
     return p_addr;
 }
